58cdf01de966a401bb06bf5a632d05e0b67f8066
vee70 <daravichett@gmail.com>
2023-11-04 22:03:07 +0700

    add getURLsFromHTML function

--- crawl.js
+++ crawl.js
@@ -1,8 +1,27 @@
+const { JSDOM } = require("jsdom");
+
+
 function normalizeURL(url) {
   const urlObj = new URL(url)
   return `${urlObj.hostname}${urlObj.pathname}`.replace(/(\/$)/, '')
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody)
+  const baseUrlObj = new URL(baseURL)
+  const queryResults = dom.window.document.querySelectorAll('a')
+  const urls = []
+  for (const url of queryResults) {
+    const urlObj = new URL(url.href)
+    if (baseUrlObj.host === urlObj.host) {
+      urls.push(urlObj.href)
+    }
+  }
+  return urls
+}
+
+
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
