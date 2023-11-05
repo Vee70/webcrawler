1e583989ba23f2b78b6a76545abf8a24b04310b8
vee70 <daravichett@gmail.com>
2023-11-05 20:25:43 +0700

    update main function to read url from commandline

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const { crawlPage } = require('./crawl.js')
+
+function main() {
+  if (process.argv.length != 3) {
+    console.log('Invalid input.')
+    process.exitCode = 1
+    return
+  }
+  const baseURL = process.argv[2]
+  console.log(baseURL)
+  crawlPage(baseURL)
+}
+
+main()
+
+
+
